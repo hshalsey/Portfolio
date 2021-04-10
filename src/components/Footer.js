a140a2a5fbb5e03cf7664f1d6adf2c72db207a69
Haley Halsey <haleyhalsey@yahoo.com>
2021-04-10 11:02:59 -0700

    basic layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -27,8 +27,7 @@ const Footer = () => {
             ))}
           </div>
           <span>
-            Made With <icon>❤</icon> by{" "}
-            <a href="https://www.chetanverma.com/">Chetan Verma</a>
+           
           </span>
         </div>
       </div>
